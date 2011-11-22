e5fe7d72809a3ec101cb94cec718da573721d9df
Masiar Bostanipoor <mail@bostanipoor.de>
2011-11-22 18:15:07 +0100

    next state of StringCalculator

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -2,22 +2,26 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
-    int result = 0;
-    String regexBreaks = "(\n|,)";
-    String regexDisallowed = "(,\n|\n,)";
-    Pattern pattern = Pattern.compile(regexDisallowed);
 
     public int add(String numbers) {
-        if (!numbers.equals("")) {
-            Matcher matcher = pattern.matcher(numbers);
-            if (matcher.find()) {
-                throw new IllegalArgumentException(numbers);
+
+        int result = 0;
+        String delimiter = ","; // default delimiter
+        String regexInvalidArguments = "(\n" + delimiter + "|" + delimiter + "\n)|(\\-\\d)";
+
+        if (!numbers.isEmpty()) {
+
+            if (numbers.startsWith("//")) {
+                delimiter = numbers.substring(2, 3);
+                numbers = numbers.substring(4, numbers.length());
             }
 
-            String[] split = numbers.split(regexBreaks);
-            if (split.length > 1) {
-                for (String x : split) {
-                    result = result + Integer.parseInt(x);
+            validate(regexInvalidArguments, numbers);
+
+            String[] split = numbers.split("(\n|\\" + delimiter + ")");
+            if (split.length > 0) {
+                for (String eachNumber : split) {
+                    result = result + Integer.parseInt(eachNumber);
                 }
             } else {
                 result = Integer.parseInt(numbers);
@@ -26,6 +30,22 @@ public class StringCalculator {
             return result;
         } else {
             return 0;
+        }
+    }
+
+    private void validate(String regexp, String numbers) {
+        String sd = "";
+        Pattern pattern = Pattern.compile(regexp);
+        Matcher matcher = pattern.matcher(numbers);
+        if (matcher.find()) {
+            matcher = pattern.matcher(numbers);
+            while (matcher.find()) {
+                sd = sd + matcher.group();
+            }
+            if (sd.contains("-")) {
+                numbers = sd;
+            }
+            throw new IllegalArgumentException(numbers);
         }
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,4 +1,3 @@
-import junit.framework.Assert;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
@@ -8,7 +7,6 @@ public class StringCalculatorTest {
 
 
     StringCalculator stringCalculator = new StringCalculator();
-
 
 
     @Test
@@ -27,26 +25,42 @@ public class StringCalculatorTest {
         assertEquals(3, stringCalculator.add("1,2"));
     }
 
-   @Test
+    @Test
     public void testLargeAmount() {
         assertEquals(146, stringCalculator.add("1,2,6,4,2,7,2,4,68,9,0,34,2,5"));
     }
 
 
     @Test
-    public void testBreaks(){
+    public void testBreaks() {
         assertEquals(5, stringCalculator.add("2\n3"));
     }
 
     @Test
-    public void testDisallowedBreaks(){
+    public void testDisallowedBreaks() {
         try {
             assertEquals(5, stringCalculator.add("2\n,3"));
             fail();
         } catch (Exception e) {
             assertEquals("2\n,3", e.getMessage());
-            System.out.println("Exception: the numbers contains erroneous delimiters: "+e.getMessage());
+            System.out.println("Exception: the numbers contains erroneous delimiters: " + e.getMessage());
         }
     }
 
+    @Test
+    public void testNewDelimiter() {
+        assertEquals(10, stringCalculator.add("//:\n4:6"));
+    }
+
+    @Test
+    public void testNegatives() {
+
+        try {
+            assertEquals(3, stringCalculator.add("4,6,-7,8,5,7,-9,7,5"));
+            fail();
+        } catch (Exception e) {
+            assertEquals("-7-9", e.getMessage());
+            System.out.println("Exception: no negatives allowed: " + e.getMessage());
+        }
+    }
 }
